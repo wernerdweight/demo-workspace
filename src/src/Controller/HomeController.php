858b5e8c7d92602584513c5afd2f4b6d2e531ead
ceplik14 <ceplik14@gmail.com>
2024-05-30 20:54:02 +0200

    php_1

---
+++ src/src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+    $portfolioItems = ['Packing Box', 'Bannery', 'Oblečení', 'Billboardy', 'Vizitky', 'Loga', 'Printy', 'Patterny', 'Fotky', 'Katalogy', 'Hra', 'Weby'];
+        return $this->render('home/index.html.twig', [
+            'portfolioItems'=> $portfolioItems,
+        ]);
+    }
+}
