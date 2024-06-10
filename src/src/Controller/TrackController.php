12629ab2709fb9307065bfd2e38bbc6262ab2c6b
Werner Dweight <wd@wernerdweight.com>
2024-06-10 19:28:02 +0200

    feat: send templated e-mail

--- src/src/Controller/TrackController.php
+++ src/src/Controller/TrackController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Track;
 use App\Form\TrackType;
 use App\Repository\ArtistRepository;
+use App\Service\AddTrackMailer;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,8 +15,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class TrackController extends AbstractController
 {
   #[Route('/track/add', name: 'app_track_add')]
-  public function add(Request $request, EntityManagerInterface $entityManager, ArtistRepository $artistRepository): Response
-  {
+  public function add(
+    Request $request,
+    EntityManagerInterface $entityManager,
+    ArtistRepository $artistRepository,
+    AddTrackMailer $addTrackMailer,
+  ): Response {
     $track = new Track();
     if ($request->query->has('artist_id')) {
       $artistId = $request->query->getInt('artist_id');
@@ -29,6 +34,9 @@ class TrackController extends AbstractController
     if ($form->isSubmitted() && $form->isValid()) {
       $entityManager->persist($track);
       $entityManager->flush();
+
+      $addTrackMailer->send($track);
+
       return $this->redirectToRoute('app_artist', ['id' => $track->getArtist()->getId()]);
     }
     return $this->render('track/add.html.twig', [
