bdb332fd8d88cda1f2e133a4d8f1a11380add91f
Dominik <dfabian9900@gmail.com>
2024-06-25 21:03:37 +0200

    database pulling

--- src/src/Controller/HomeController.php
+++ src/src/Controller/HomeController.php
@@ -2,15 +2,24 @@
 
 namespace App\Controller;
 
+
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Project;
 
 class HomeController extends AbstractController
 {
   #[Route('/', name: 'app_home')]
-  public function index(): Response
+  public function index(ProjectRepository $projectRepository): Response
   {
-    return $this->render('home/index.html.twig');
+   $projects = $projectRepository->findAll();
+   
+
+    
+    return $this->render('home/index.html.twig', [
+    'projects' => $projects,
+    ]);
   }
 }
