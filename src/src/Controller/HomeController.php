3a5d2359e218a04abc99189f4d8886ab37648621
Grizel H. Chernobog <zoe.hewitt666@gmail.com>
2024-07-07 16:27:16 +0200

    lets see

--- src/src/Controller/HomeController.php
+++ src/src/Controller/HomeController.php
@@ -1,16 +1,30 @@
 <?php
 
 namespace App\Controller;
+
+use App\Entity\FormEntity;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('FormEntityRepository.php', name: 'app_home')]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('home/index.html.twig');
+    if ($request->getMethod() === Request::METHOD_POST) {
+        $ratings = $request->request->get('ratings');
+        $formEntity = new FormEntity();
+        $formEntity->setFormulary($ratings);
+
+        $entityManager->persist($formEntity);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_home');
+    }
+    return $this->render('home/index.html.twig');
     }
 
 }
--- src/src/Repository/FormEntityRepository.php
+++ src/src/Repository/FormEntityRepository.php
@@ -9,7 +9,7 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @extends ServiceEntityRepository<FormEntity>
  */
-class formularycontainer extends ServiceEntityRepository
+class FormEntityRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
