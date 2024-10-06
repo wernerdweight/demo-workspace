68614063c4906a7fa9394c0facad3620dc576f6d
Werner Dweight <wd@wernerdweight.com>
2024-10-06 22:35:05 +0200

    chore: refresh

--- src/public/index.php
+++ src/public/index.php
@@ -1,112 +1,49 @@
-<?php
-
-declare(strict_types=1);
-
-require_once dirname(__DIR__) . '/vendor/autoload.php';
-
-use App\MenuItem;
-use App\Request\TestRequest;
-
-$user = 'Franta';
-//$user = null;
-$isAuthorized = $user !== null; // value is false
-
-echo MenuItem::getCount() . PHP_EOL;  // 0
-
-$menuItems = [
-  new MenuItem("Home", "/"),
-  new MenuItem("About me", "/about.html"),
-  new MenuItem("Secret chamber", "/secret.html", $isAuthorized),
-  new MenuItem("Logout", "/logout", $isAuthorized),
-  new MenuItem("Login", "/login", !$isAuthorized),
-];
-
-echo MenuItem::getCount() . PHP_EOL; // 5
-
-$menuItems[] = new MenuItem("Contact", "/contact.html");
-
-echo MenuItem::getCount() . PHP_EOL; // 6
-
-unset($menuItems[5]);
-
-echo MenuItem::getCount() . PHP_EOL; // 5
-
-
-
-$test = new MenuItem("Test", "/test.html", false);
-
-echo MenuItem::getCount() . PHP_EOL; // 6
-echo count($menuItems) . PHP_EOL; // 5
-
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Mailhog Test</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Mailhog Test</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-<div class="container">
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="/"><?=$user ?: 'anonymous' ?></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+  <div class="container">
+    <h1>Smoke Test</h1>
+    <p>Check the mailhog web interface at <a href="http://localhost:8025/" target="_blank">http://localhost:8025/</a> to see the email.</p>
+    <p>Check the adminer web interface at <a href="http://localhost:8080/" target="_blank">http://localhost:8080/</a> to access the database.</p>
 
-          <?php
-          foreach ($menuItems as $menuItem) {
-            if (!$menuItem->shouldDisplay()) { // equivalent of $menuItem->shouldDisplay === false
-              continue;
-            }
-          ?>
-            <li class="nav-item">
-              <a
-                class="nav-link <?= $menuItem->isActive() ? 'active' : '' ?>"
-                href="<?php echo $menuItem->link; ?>"
-              >
-                <?php echo $menuItem->name; ?>
-              </a>
-            </li>
-          <?php
-          }
-          ?>
+    <div class="text-bg-dark">
+    <pre>
+      <div class="container">
 
-        </ul>
+<?php
+
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mime\Email;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
+// send an email to test the mailhog container SMTP server
+echo 'sending test email...' . PHP_EOL;
+
+$transport = Transport::fromDsn('smtp://mailhog:1025');
+$mailer = new Mailer($transport);
+
+$email = (new Email())
+    ->from('hello@example.com')
+    ->to('you@example.com')
+    ->subject('Test Message!')
+    ->text('Not a real message. Just testing the mailhog container.');
+
+$mailer->send($email);
+
+echo 'sent' . PHP_EOL;
+
+?>
       </div>
+    </pre>
     </div>
-  </nav>
-  <form method="post" action="/" enctype="multipart/form-data">
-    <div class="mb-3">
-      <label for="name" class="form-label">Name</label>
-      <input type="text" class="form-control" id="name" name="name">
-    </div>
-    <div class="mb-3">
-      <label for="email" class="form-label">Email</label>
-      <input type="email" class="form-control" id="email" name="whatever">
-    </div>
-    <div class="mb-3">
-      <label for="audio" class="form-label">Audio file</label>
-      <input type="file" class="form-control" id="audio" name="audio">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
-  <pre>
-    <?php
-    $request = new TestRequest();
-    $request->logGetParams();
-    $request->logPostParams();
-    $request->logFiles();
-    $request->logServerParams();
-
-    echo $_POST['whatever'];
-    ?>
-  </pre>
-</div>
+  </div>
 </body>
 </html>
