e7b05d110c5838f7c648a74611c0e596d05ab87b
Lukas Voplakal <voplakal@shoptet.cz>
2024-10-23 10:11:44 +0200

    First javascript

--- project/js/script.js
+++ project/js/script.js
@@ -1,7 +1,48 @@
-document.querySelector('.setting img').addEventListener('mouseenter', function() {
-    this.src = '/Users/lukasvoplakal/coding-school/demo-workspace/project/html/img/settings.svg'; // Cesta k obrázku při hover
+const sideLinksId = ['settings', 'home', 'personal', 'work', 'search']
+
+sideLinksId.forEach ((sideLinkId) => {
+    const url = '/Users/lukasvoplakal/coding-school/demo-workspace/project/html/img/'
+    document.querySelector('#' + sideLinkId + '_icon').addEventListener('mouseenter', function() {
+        this.src = url + sideLinkId + '.svg'
+    });
+    document.querySelector('#' + sideLinkId + '_icon').addEventListener('mouseleave', function() {
+        this.src = url + sideLinkId + '_hover' + '.svg'
+    });
 });
 
-document.querySelector('.setting img').addEventListener('mouseleave', function() {
-    this.src = '/Users/lukasvoplakal/coding-school/demo-workspace/project/html/img/settings_hover.svg'; // Původní obrázek při odchodu
-});
+// document.querySelector('#settings_icon').addEventListener('mouseenter', function() {
+//     this.src = '/Users/lukasvoplakal/coding-school/demo-workspace/project/html/img/settings.svg';
+// });
+
+// document.querySelector('#settings_icon').addEventListener('mouseleave', function() {
+//     this.src = '/Users/lukasvoplakal/coding-school/demo-workspace/project/html/img/settings_hover.svg';
+// });
+
+// document.querySelector('#home_icon').addEventListener('mouseenter', function() {
+//     this.src = '/Users/lukasvoplakal/coding-school/demo-workspace/project/html/img/home.svg';
+// });
+
+// document.querySelector('#home_icon').addEventListener('mouseleave', function() {
+//     this.src = '/Users/lukasvoplakal/coding-school/demo-workspace/project/html/img/home_hover.svg';
+// });
+// document.querySelector('#personal_icon').addEventListener('mouseenter', function() {
+//     this.src = '/Users/lukasvoplakal/coding-school/demo-workspace/project/html/img/personal.svg';
+// });
+
+// document.querySelector('#personal_icon').addEventListener('mouseleave', function() {
+//     this.src = '/Users/lukasvoplakal/coding-school/demo-workspace/project/html/img/personal_hover.svg';
+// });
+// document.querySelector('#work_icon').addEventListener('mouseenter', function() {
+//     this.src = '/Users/lukasvoplakal/coding-school/demo-workspace/project/html/img/work.svg'; 
+// });
+
+// document.querySelector('#work_icon').addEventListener('mouseleave', function() {
+//     this.src = '/Users/lukasvoplakal/coding-school/demo-workspace/project/html/img/work_hover.svg';
+// });
+// document.querySelector('#search_icon').addEventListener('mouseenter', function() {
+//     this.src = '/Users/lukasvoplakal/coding-school/demo-workspace/project/html/img/search.svg'; 
+// });
+
+// document.querySelector('#search_icon').addEventListener('mouseleave', function() {
+//     this.src = '/Users/lukasvoplakal/coding-school/demo-workspace/project/html/img/search_hover.svg';
+// });
