9249aea2a8b3d3d436fdc68ac295e0a8525dba77
josef41313 <josef.nemecek07@seznam.cz>
2024-11-05 20:45:50 +0100

    add framework all

---
+++ src/src/Entity/Ingredient.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IngredientRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: IngredientRepository::class)]
+class Ingredient
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nameIng = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $amount = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $unit = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNameIng(): ?string
+    {
+        return $this->nameIng;
+    }
+
+    public function setNameIng(string $nameIng): static
+    {
+        $this->nameIng = $nameIng;
+
+        return $this;
+    }
+
+    public function getAmount(): ?string
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(?string $amount): static
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getUnit(): ?string
+    {
+        return $this->unit;
+    }
+
+    public function setUnit(?string $unit): static
+    {
+        $this->unit = $unit;
+
+        return $this;
+    }
+}
---
+++ src/src/Entity/Recipe.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecipeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RecipeRepository::class)]
+class Recipe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $prepTime = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrepTime(): ?int
+    {
+        return $this->prepTime;
+    }
+
+    public function setPrepTime(?int $prepTime): static
+    {
+        $this->prepTime = $prepTime;
+
+        return $this;
+    }
+}
