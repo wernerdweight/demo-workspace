7f5d6e06ed4a4bd00ca1fdb76e1b325da8b885e2
Ivana Pluharikova <pluharikova@shoptet.cz>
2024-11-16 10:18:39 +0100

    last changes before remake

--- src/src/Controller/PageController.php
+++ src/src/Controller/PageController.php
@@ -25,16 +25,13 @@ class PageController extends AbstractController
     #[Route('/game/{latitude}/{longitude}', name: 'game')]
     public function game(float $latitude, float $longitude, EntityManagerInterface $em): Response //tady to musíš opravit
     {
-        // Vyhledáme text pro konkrétní souřadnice
         $locationText = $em->getRepository(LocationText::class)->findOneBy([
             'latitude' => $latitude,
             'longitude' => $longitude,
         ]);
 
-        // Pokud text pro dané souřadnice není nalezen, použijeme výchozí text
         $text = $locationText ? $locationText->getText() : 'Text pro tuto lokaci nebyl nalezen.';
 
-        // Předáme souřadnice a text do šablony
         return $this->render('game.html.twig', [
             'latitude' => $latitude,
             'longitude' => $longitude,
